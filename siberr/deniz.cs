77668587836e6227e2231d77aa2a78639e8f76df
nevzt <nevzatulas1955@gmail.com>
2025-11-04 21:47:56 +0300

    a

--- siberr/deniz.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.Write("HELLO WORLD!");
-Console.WriteLine(" This is Siberr project.");
-int sayi1 = 15;
-int sayi2 = 16;
-int mast = sayi1 + sayi2;
-Console.WriteLine("MAST: " + mast);
-Console.WriteLine("denız love hatay");
