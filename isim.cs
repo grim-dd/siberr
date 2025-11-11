7cd7053be294f1a4d5de44c0fba9b4e8389be591
nevzt <91660598+nevztab@users.noreply.github.com>
2025-11-11 18:04:46 +0300

    a

--- isim.cs
+++ isim.cs
@@ -1,184 +1,200 @@
 // See https://aka.ms/new-console-template for more information
-EnBuyukSayi();
-
-/*
-Console.WriteLine("Hello, World!");
-Console.Write("Ad : ");
-Console.WriteLine("Deniz");
-Console.Write("Soyad : ");
-Console.WriteLine("Vayısoğlu"); */
-
-/*
-string isim = "Ahmet";
-int yas = 18;
-Console.WriteLine($"İsim : {isim}, Yaş : {yas}"); */
-
-/*
-int a = 10;
-int b = 20;
-Console.WriteLine($"Toplam : {a+b}"); */
 
 
-// GİRİLEN İKİ SAYIYI TOPLAMA
-/*
-Console.Write("Birinci sayıyı giriniz: ");
-int s1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("İkinci sayıyı giriniz: ");
-int s2 = Convert.ToInt32(Console.ReadLine());
-
-int toplam = s1 + s2;
-Console.WriteLine("Toplam = " + toplam); */
-
-
-
-// DOGUM TARİHİ GİRİLEN BİRİNİN YAŞINI HESAPLAMA
-/*
-Console.Write("Doğum Yılınızı Giriniz : ");
-int dogum = Convert.ToInt32(Console.ReadLine());
-
-int yas = 2025 - dogum;
-Console.WriteLine("Yaşınız : " + yas); */
-
-
-// GİRİLEN 3 SINAV NOTUNUN ORTALAMASINI HESAPLAMA
-/*
-Console.Write("Sınav 1 : ");
-double sınav1 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Sınav 2 : ");
-double sınav2 = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Sınav 3 : ");
-double sınav3 = Convert.ToDouble(Console.ReadLine());
-
-double ort = (sınav1 + sınav2 + sınav3) / 3;
-Console.Write($"Ortalamanız : {ort} "); */
-
-
-// GİRİLEN 2 SAYININ ARİTMETİK İŞLEMLERİNİ YAPMA
-/*
-Console.Write("Sayı 1 : ");
-int sayi1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Sayı 2 : ");
-int sayi2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Toplam = " + (sayi1 + sayi2));
-Console.WriteLine("Fark   = " + (sayi1 - sayi2));
-Console.WriteLine("Çarpım = " + (sayi1 * sayi2));
-Console.WriteLine("Bölüm  = " + (sayi1 / sayi2));
-Console.WriteLine("Kalan  = " + (sayi1 % sayi2)); */
-
-
-// YAŞI GİRİLEN BİRİNİN REŞİT OLUP OLMADIĞINI KONTROL ETME
-/*Console.Write("Yaşınız = ");
-int yas = Convert.ToInt32(Console.ReadLine());
-if(yas >= 18)
+void EkaranaYazdirma()
 {
-    Console.WriteLine("Reşitsiniz.");
+    Console.WriteLine("Hello, World!");
+    Console.Write("Ad : ");
+    Console.WriteLine("Deniz");
+    Console.Write("Soyad : ");
+    Console.WriteLine("Vayısoğlu");
+
 }
-else
+void FarkliVerileriToplama()
 {
-    Console.WriteLine("Reşit Değilsiniz.");
+    string isim = "Ahmet";
+    int yas = 18;
+    Console.WriteLine($"İsim : {isim}, Yaş : {yas}");
 }
-*/
-
-
-// GİRİLEN SAYININ POZİTİF, NEGATİF VEYA NÖTR OLDUĞUNU KONTROL ETME
-/*Console.Write("Sayi = ");
-int sayi = Convert.ToInt32(Console.ReadLine());
-
-if (sayi > 0)
+void AyniVerileriToplama()
 {
-    Console.WriteLine("Pozitif");
+    int a = 10;
+    int b = 20;
+    Console.WriteLine($"Toplam : {a + b}");
 }
-else if (sayi < 0)
+
+void GirilenİlkSayiyiBulma()
 {
-    Console.WriteLine("Negatif");
+    Console.Write("Birinci sayıyı giriniz: ");
+    int s1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("İkinci sayıyı giriniz: ");
+    int s2 = Convert.ToInt32(Console.ReadLine());
+
+    int toplam = s1 + s2;
+    Console.WriteLine("Toplam = " + toplam);
 }
-else 
-{ 
-    Console.WriteLine("Nötr");
-}
-*/
 
-
-// GİRİLEN NOTA GÖRE GEÇME SEVİYESİNİ BELİRLEME
-/*Console.Write("Not : ");
-int not = int.Parse(Console.ReadLine());
-
-if (not >= 85)
+void DogumTarihleriGirilenBirininYasiniHesaplama()
 {
-    Console.WriteLine("PEKİYİ");
+    Console.Write("Doğum Yılınızı Giriniz : ");
+    int dogum = Convert.ToInt32(Console.ReadLine());
+
+    int yas = 2025 - dogum;
+    Console.WriteLine("Yaşınız : " + yas); 
 }
-else if (not >= 70)
+
+void GirilenUcSinavNotununOrtalamasi()
 {
-    Console.WriteLine("İYİ");
+    Console.Write("Sınav 1 : ");
+    double sınav1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Sınav 2 : ");
+    double sınav2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Sınav 3 : ");
+    double sınav3 = Convert.ToDouble(Console.ReadLine());
+    double ort = (sınav1 + sınav2 + sınav3) / 3;
+    Console.Write($"Ortalamanız : {ort} ");
 }
-else if (not >= 50)
+
+
+
+void GirilenIkiSayininAritmetikIslemleri()
 {
-    Console.WriteLine("ORTA");
+    Console.Write("Sayı 1 : ");
+    int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Sayı 2 : ");
+    int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Toplam = " + (sayi1 + sayi2));
+    Console.WriteLine("Fark   = " + (sayi1 - sayi2));
+    Console.WriteLine("Çarpım = " + (sayi1 * sayi2));
+    Console.WriteLine("Bölüm  = " + (sayi1 / sayi2));
+    Console.WriteLine("Kalan  = " + (sayi1 % sayi2));
 }
-else
+
+
+void YasiGirilenKisininResitOlupOlmadiginiKontrolEtme()
 {
-    Console.WriteLine("KALDINIZ");
-}
-*/
-
-
-// KULLANICI ADI, ŞİFRE VE AKTİF/PASİF DURUMUNA GÖRE GİRİŞ KONTROLÜ YAPMA
-/*
-Console.Write("Kullanıcı Adınız : ");
-string kullaniciAdi = Console.ReadLine();
-
-Console.Write("Şifreniz : ");
-string sifre = Console.ReadLine();
-
-Console.Write("Aktif/Pasif durumunu giriniz : true veya false olarak : ");
-bool aktif = bool.Parse(Console.ReadLine());
-
-
-if (kullaniciAdi == "admin")
-{
-    if (sifre == "12345")
+    Console.Write("Yaşınız = ");
+    int yas = Convert.ToInt32(Console.ReadLine());
+    if (yas >= 18)
     {
-        if (aktif==true)
-        {
-            Console.WriteLine("Hoş Geldiniz Admin!");
-        }
-        else
-        {
-            Console.WriteLine("Hesabınız Aktif Değil");
-        }
+        Console.WriteLine("Reşitsiniz.");
     }
     else
     {
-        Console.WriteLine($"Hatalı şifre!");
+        Console.WriteLine("Reşit Değilsiniz.");
     }
 }
-else
-    Console.WriteLine("Hatalı kullanıcı girişi");
-*/
+
+
+
+
+// GİRİLEN SAYININ POZİTİF, NEGATİF VEYA NÖTR OLDUĞUNU KONTROL ETME
+
+void SayiPozitifNegatifNötr()
+
+{
+    Console.Write("Sayi = ");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    
+    if (sayi > 0)
+    {
+        Console.WriteLine("Pozitif");
+    }
+    else if (sayi < 0)
+    {
+        Console.WriteLine("Negatif");
+    }
+    else
+    {
+        Console.WriteLine("Nötr");
+    }
+}
+
+
+// GİRİLEN NOTA GÖRE GEÇME SEVİYESİNİ BELİRLEME
+void GirilenNotaGoreGecmeSeviyesi()
+{
+    Console.Write("Not : ");
+    int not = int.Parse(Console.ReadLine());
+
+    if (not >= 85)
+    {
+        Console.WriteLine("PEKİYİ");
+    }
+    else if (not >= 70)
+    {
+        Console.WriteLine("İYİ");
+    }
+    else if (not >= 50)
+    {
+        Console.WriteLine("ORTA");
+    }
+    else
+    {
+        Console.WriteLine("KALDINIZ");
+    }
+}
+
+
+// KULLANICI ADI, ŞİFRE VE AKTİF/PASİF DURUMUNA GÖRE GİRİŞ KONTROLÜ YAPMA
+
+void KullanıcıAdiSifreAktiflikKontrol() {
+    Console.Write("Kullanıcı Adınız : ");
+    string kullaniciAdi = Console.ReadLine();
+
+    Console.Write("Şifreniz : ");
+    string sifre = Console.ReadLine();
+
+    Console.Write("Aktif/Pasif durumunu giriniz : true veya false olarak : ");
+    bool aktif = bool.Parse(Console.ReadLine());
+
+
+    if (kullaniciAdi == "admin")
+    {
+        if (sifre == "12345")
+        {
+            if (aktif == true)
+            {
+                Console.WriteLine("Hoş Geldiniz Admin!");
+            }
+            else
+            {
+                Console.WriteLine("Hesabınız Aktif Değil");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Hatalı şifre!");
+        }
+    }
+    else
+        Console.WriteLine("Hatalı kullanıcı girişi");
+}
 
 
 // YAŞI VE EHLİYET DURUMUNA GÖRE ARAÇ KULLANIP KULLANAMAYACAĞINI BELİRLEME
-/*
-Console.Write("Yaşınız : ");
-int yas = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Ehliyetiniz var mı : varsa true yoksa false değeriniz giriniz.  :  ");
-bool ehliyet = bool.Parse(Console.ReadLine());
+void YasVeEhliyetDurumu()
+{
+    Console.Write("Yaşınız : ");
+    int yas = Convert.ToInt32(Console.ReadLine());
 
-if (yas >= 18 && ehliyet == true )
-{
-    Console.WriteLine("Araç Kullanabilirsiniz.");
+    Console.Write("Ehliyetiniz var mı : varsa true yoksa false değeriniz giriniz.  :  ");
+    bool ehliyet = bool.Parse(Console.ReadLine());
+
+    if (yas >= 18 && ehliyet == true)
+    {
+        Console.WriteLine("Araç Kullanabilirsiniz.");
+    }
+    else
+    {
+        Console.WriteLine("Araç Kullanamazsınız.");
+    }
 }
-else
-{
-    Console.WriteLine("Araç Kullanamazsınız.");
-}
-*/
+
 
 
 
@@ -207,7 +223,6 @@ void EnBuyukSayi()
     }
 
 }
-
 
 
 
